5a21cac8ccddac2cae9d05fa4bdabfb534a3477f
codestudy25 <codestudy25@gmail.com>
2024-01-15 17:39:09 +0000

    feat - Data - data augment code update
    #32

--- augmentation/mlm_augmentation.py
+++ augmentation/mlm_augmentation.py
@@ -2,8 +2,22 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 import pandas as pd
 
+import torch
+
 import json
 import random
+from tqdm import tqdm
+
+def find_word_indices(sentence, word):
+    indices = []
+    start_index = 0
+    while True:
+        index = sentence.find(word, start_index)
+        if index == -1:
+            break
+        indices.append((index, index + len(word) - 1))
+        start_index = index + len(word) - 1
+    return indices
 
 
 def main(config):
@@ -19,8 +33,8 @@ def main(config):
     target_df = target_df.reset_index(drop=True)
     
     
-    model = AutoModelForMaskedLM.from_pretrained(config['model_name'])
-    tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
+    model = AutoModelForMaskedLM.from_pretrained(config['model_name'],cache_dir='/data/ephemeral/tmp')
+    tokenizer = AutoTokenizer.from_pretrained(config['model_name'],cache_dir='/data/ephemeral/tmp')
     
     new_index_ids = []
     new_sentences = []
@@ -29,7 +43,7 @@ def main(config):
     new_labels = []
     new_sources = []
     
-    for i in range(len(target_df)):
+    for i in tqdm(range(len(target_df))):
         data = target_df.iloc[i]
         
         index_id = data["id"]
@@ -49,21 +63,72 @@ def main(config):
         sentence = sentence.replace(subject_word,subject_word_one)
         sentence = sentence.replace(object_word,object_word_one)
         
-        sentence_split = sentence.split()
-        sentence_word_num = len(sentence_split)
-        
-        for i in range(4):
-            random_loc = random.randint(0,sentence_word_num+i-1)
+        for i in range(config["masking_num"]):
+            sentence_split = sentence.split()
+            random_loc = random.randint(0,len(sentence_split)-1)
             sentence_split.insert(random_loc, "[MASK]")
             sentence = " ".join(sentence_split)
             
-            tokenized_sentence = tokenizer.encode(sentence)
-            logits = model(tokenized_sentence).logits
+            tokens = tokenizer(sentence, return_tensors="pt")
+
+            mask_index = torch.where(tokens["input_ids"] == tokenizer.mask_token_id)
+
+            with torch.no_grad():
+                outputs = model(**tokens)
+                predictions = outputs.logits[:, mask_index[1], :].argmax(dim=-1)
+
+            predicted_token = tokenizer.convert_ids_to_tokens(predictions.item())
+            sentence = sentence.replace("[MASK]", predicted_token)
         
+
+        sentence = sentence.replace(subject_word_one, subject_word)
+        sentence = sentence.replace(object_word_one, object_word)
+
+        new_sentence = sentence 
+        subj = eval(subject_entity)["word"]
+        obj = eval(object_entity)["word"]
+        sub_type = eval(object_entity)["type"]
+        obj_type = eval(object_entity)["type"]
+        
+        subj_indices = find_word_indices(new_sentence, subj)
+        obj_indices = find_word_indices(new_sentence, obj)
+        
+        for sub_start_idx, sub_end_idx in subj_indices:
+            for obj_start_idx, obj_end_idx in obj_indices:
+                new_subj_entity = str(
+                    {
+                        "word": subj,
+                        "start_idx": sub_start_idx,
+                        "end_idx": sub_end_idx,
+                        "type": sub_type,
+                    }
+                )
+                new_obj_entity = str(
+                    {
+                        "word": obj,
+                        "start_idx": obj_start_idx,
+                        "end_idx": obj_end_idx,
+                        "type": obj_type,
+                    }
+                )
+                new_index_ids.append(index_id)
+                new_sentences.append(new_sentence)
+                new_subject_entities.append(new_subj_entity)
+                new_object_entities.append(new_obj_entity)
+                new_labels.append(label)
+                new_sources.append(source)
+    new_data = pd.DataFrame(
+        {
+            "id": new_index_ids,
+            "sentence": new_sentences,
+            "subject_entity": new_subject_entities,
+            "object_entity": new_object_entities,
+            "label": new_labels,
+            "souce": new_sources,
+        }
+    )
     
-
-
-
+    new_data.to_csv('./dataset/'+config['model_name'].split("/")[-1]+'-'+str(config['augment_target'])+'-'+str(config['masking_num'])+'.csv')
 
 if __name__ == '__main__':
     with open(f'./configs/mlm_augmentation_config.json', 'r') as f:
